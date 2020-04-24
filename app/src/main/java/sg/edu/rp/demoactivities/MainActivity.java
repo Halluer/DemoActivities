9a17c6ea57dd32ac4538df8477e0d1941f309230
18055857 <18055857@myrp.edu.sg>
2020-04-24 14:26:18 +0800

    Demoactivity update

--- app/src/main/java/sg/edu/rp/demoactivities/MainActivity.java
+++ app/src/main/java/sg/edu/rp/demoactivities/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+//Elgin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
